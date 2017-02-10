f6e7add5ccb5954e34dc99c0e55c365123a7ebe7
Aaron Young <aarony422@gmail.com>
2017-02-09 19:53:49 -0600

    displayTodo shows text and completed status

--- todo.js
+++ todo.js
@@ -1,8 +1,20 @@
-/* Version 4 */
+/* Version 5 */
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log("My todos:", this.todos);
+    if (this.todos.length === 0) {
+      console.log("Your Todos is empty!");
+    } else {
+      console.log("My Todos:");
+      for (var i = 0; i < this.todos.length; i++) {
+        var item = this.todos[i];
+        if (item.completed === true) {
+          console.log("[x]", item.todoText);
+        } else {
+          console.log("[ ]", item.todoText);
+        }
+      }
+    }
   },
   addTodo: function(todoText) {
     this.todos.push({
@@ -25,6 +37,7 @@ var todoList = {
     this.displayTodos();
   }
 };
+
 
 
 /* Version 2
