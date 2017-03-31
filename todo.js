e4c786f8b4ba996a859e8079afb6f9c4599ae199
Aaron Young <aaronyoung@Aarons-MacBook-Pro.local>
2017-03-31 15:28:45 -0500

    modularized displayTodos, and separated Model, View, Controller components

--- todo.js
+++ todo.js
@@ -1,43 +1,21 @@
 $(document).ready(function(){
+  // Model
   var todoList = {
     todos: [],
-    displayTodos: function() {
-      //var ul = document.getElementById("todosDisplay");
-      //var li = document.createElement("li");
-      //li.appendChild(document.createTextNode("Four"));
-      //li.setAttribute("id", "element4"); // added line
-      //ul.appendChild(li);
-      if (this.todos.length === 0) {
-        console.log("Your Todos is empty!");
-      } else {
-        console.log("My Todos:");
-        for (var i = 0; i < this.todos.length; i++) {
-          var item = this.todos[i];
-          if (item.completed === true) {
-            console.log("[x]", item.todoText);
-          } else {
-            console.log("[ ]", item.todoText);
-          }
-        }
-      }
-    },
     // append a todo object to the todoList with todoText
     addTodo: function(todoText) {
       this.todos.push({
         todoText: todoText,
         completed: false
       });
-      this.displayTodos();
     },
     // change todoText of todo object at a given position
     changeTodo: function(position, newTodoText) {
       this.todos[position].todoText = newTodoText;
-      this.displayTodos();
     },
     // splice out the todo object at given position
     deleteTodo: function(position) {
       this.todos.splice(position, 1);
-      this.displayTodos();
     },
     // toggles completed for all todo objects
     // toggles all to false if all todos are completed
@@ -45,7 +23,6 @@ $(document).ready(function(){
     toggleCompleted: function(position) {
       var todo = this.todos[position];
       todo.completed = !todo.completed;
-      this.displayTodos();
     },
     toggleAll: function() {
       var totalTodos = this.todos.length;
@@ -66,19 +43,35 @@ $(document).ready(function(){
           this.todos[i].completed = true;
         }
       }
-      this.displayTodos();
     }
   };
 
-  // Event handlers
-  // displayTodos:
-  $("#displayTodos").click(function() {
-    todoList.displayTodos();
-  });
+  // View
+  var view = {
+    displayTodos: function() {
+      // clear todosDisplay
+      var ul = $("#todosDisplay");
+      ul.empty();
+
+      if (todoList.todos.length !== 0) {
+        for (var i = 0; i < todoList.todos.length; i++) {
+          var item = todoList.todos[i];
+          if (item.completed === true) {
+            ul.append('<li>[x] ' + item.todoText + '</li>');
+          } else {
+            ul.append('<li>[ ] ' + item.todoText + '</li>');
+          }
+        }
+      }
+    }
+  };
+
+  // Controller (Event handlers)
 
   // toggleAll
   $("#toggleAll").click(function() {
     todoList.toggleAll();
+    view.displayTodos();
   });
 
   // Add todos
@@ -86,6 +79,7 @@ $(document).ready(function(){
     var addTodoTextInput = $("#addTodoTextInput");
     todoList.addTodo(addTodoTextInput.val());
     addTodoTextInput.val('');
+    view.displayTodos();
   });
 
   // changeTodo
@@ -95,6 +89,7 @@ $(document).ready(function(){
     todoList.changeTodo(parseInt(changeTodoPositionInput.val(), 10), changeTodoTextInput.val());
     changeTodoPositionInput.val('');
     changeTodoTextInput.val('');
+    view.displayTodos();
   });
 
   // deleteTodo
@@ -102,6 +97,7 @@ $(document).ready(function(){
     var deleteTodoPositionInput = $("#deleteTodoPositionInput");
     todoList.deleteTodo(parseInt(deleteTodoPositionInput.val(), 10));
     deleteTodoPositionInput.val('');
+    view.displayTodos();
   });
 
   // toggleCompleted
@@ -109,5 +105,6 @@ $(document).ready(function(){
     var toggleCompletedInput = $('#toggleCompletedInput');
     todoList.toggleCompleted(parseInt(toggleCompletedInput.val(),10));
     toggleCompletedInput.val('');
+    view.displayTodos();
   });
 });
